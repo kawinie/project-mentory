ea677a0deffefce72b4c2e5e9773f4d0b82e86d6
Kawin <pechetrk@oregonstate.edu>
2020-12-15 22:16:36 +0700

    Added @tailwindcss/custom-forms

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,23 @@
-const colors = require("tailwindcss/colors");
-
 module.exports = {
+    purgecss: {
+        content: [
+            "./pages/**/*.tsx",
+            "./pages/**/*.html",
+            "./components/**/*.tsx",
+            "./layouts/**/*.tsx",
+        ],
+    },
     darkMode: false, // or 'media' or 'class'
     theme: {
         extend: {
             // Enable all Tailwind color utilities
-            colors: colors,
+            colors: require("tailwindcss/colors"),
         },
     },
     variants: {
         extend: {},
     },
-    plugins: [],
+    plugins: [require("@tailwindcss/custom-forms")],
     screen: {
         "2xl": false,
     },
