520d9a5146e36f14fc51d4be6618f5eb55b41f4d
Kawin <pechetrk@oregonstate.edu>
2020-12-15 18:21:38 +0700

    Updated _app and _document to support styled-components

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,15 +3,19 @@ import Head from "next/head";
 import { UnitExample } from "components/units/UnitExample";
 import { ReactElement } from "react";
 
+import tw from "twin.macro";
+
+const HomeContainer = tw.div`flex items-center justify-center w-full h-screen text-5xl text-gray-700 bg-gray-200`;
+
 export default function Home(): ReactElement {
     return (
         <>
             <Head>
-                <title>Mentory</title>
+                <title>Mentory ({process.env.NODE_ENV == "development" && "development"})</title>
             </Head>
-            <div className="flex items-center justify-center w-full h-screen text-5xl text-gray-700 bg-gray-200 ">
+            <HomeContainer>
                 <UnitExample title="Mentory" />
-            </div>
+            </HomeContainer>
         </>
     );
 }
