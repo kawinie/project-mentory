f34778abbb6e24889638cd300c8ca4ef6b64b3c6
Kawin <pechetrk@oregonstate.edu>
2020-12-17 22:45:38 +0700

    Injected GlobalStyles to make Tailwind work

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,4 +1,17 @@
+import React from 'react';
+
+import { GlobalStyles } from 'twin.macro';
+
+// Global decorator to apply the styles to all stories
+export const decorators = [
+  Story => (
+    <>
+      <GlobalStyles />
+      <Story />
+    </>
+  ),
+];
 
 export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
-}
+  actions: { argTypesRegex: '^on[A-Z].*' },
+};
