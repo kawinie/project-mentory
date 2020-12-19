a84eee0c056f2f336e6fdf5329e3612851119d9a
Kawin <pechetrk@oregonstate.edu>
2020-12-20 01:44:22 +0700

    Enabled ESLint for .storybook and absolute imports

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,10 +1,12 @@
+const path = require("path");
+
 module.exports = {
-  "stories": [
-    "../stories/**/*.stories.mdx",
-    "../stories/**/*.stories.@(js|jsx|ts|tsx)"
-  ],
-  "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials"
-  ]
-}
+    stories: ["../stories/**/*.stories.mdx", "../stories/**/*.stories.@(js|jsx|ts|tsx)"],
+    addons: ["@storybook/addon-links", "@storybook/addon-essentials"],
+    webpackFinal: async (config) => {
+        console.log("Webpack Config");
+        console.log(config);
+        config.resolve.modules = [path.resolve(__dirname, ".."), "node_modules"];
+        return config;
+    },
+};
--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,18 +1,18 @@
-import React from 'react';
+import React from "react";
 
-import { GlobalStyles } from 'twin.macro';
+import { GlobalStyles } from "twin.macro";
 
 // Global decorator to apply the styles to all stories
 export const decorators = [
-  Story => (
-    <>
-      <GlobalStyles />
-      <Story />
-    </>
-  ),
+    (Story) => (
+        <>
+            <GlobalStyles />
+            <Story />
+        </>
+    ),
 ];
 
 export const parameters = {
-  actions: { argTypesRegex: '^on[A-Z].*' },
-  layout: "centered"
+    actions: { argTypesRegex: "^on[A-Z].*" },
+    layout: "centered",
 };
