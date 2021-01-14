0cfb3d7fc2f018e5aea94a29f8a23f6dda3bf402
Kawin <pechetrk@oregonstate.edu>
2021-01-14 20:18:57 +0700

    Updated mentor cover image with square image

--- pages/mentor.tsx
+++ pages/mentor.tsx
@@ -1,6 +1,7 @@
-import { Box, Flex, VStack, HStack, Grid, Heading, Text, Badge } from "@chakra-ui/react";
 import "twin.macro";
 import Image from "next/image";
+import { Box, Flex, VStack, HStack, Grid, Text, Badge, AspectRatio } from "@chakra-ui/react";
+import { times } from "lodash";
 
 type TabButtonProps = {
     isActive?: boolean;
@@ -17,65 +18,69 @@ function TabButton({ isActive }: TabButtonProps) {
     );
 }
 
+type AttributesProps = {
+    title: string;
+    badges: string[];
+};
+
+function Attributes({ title, badges }: AttributesProps) {
+    return (
+        <VStack align="start">
+            <Text tw="uppercase text-xs font-bold">{title}</Text>
+            <HStack>
+                {badges.map((text) => (
+                    <Badge key={text} tw="rounded-md px-2 py-1 font-medium" color="gray.500">
+                        {text}
+                    </Badge>
+                ))}
+            </HStack>
+        </VStack>
+    );
+}
+
+function ImageDisplay() {
+    return (
+        <VStack w="full" align="start" spacing={4}>
+            <AspectRatio tw="relative w-full" ratio={1} bgColor="gray.100">
+                {/* sizes=100% is needed here to make next/image work properly */}
+                <Image src="/svg/avatar.svg" sizes="100%" layout="fill" objectFit="contain" />
+            </AspectRatio>
+            <HStack px={4} spacing={2}>
+                <TabButton isActive />
+                <TabButton />
+                <TabButton />
+                <TabButton />
+                <TabButton />
+            </HStack>
+        </VStack>
+    );
+}
+
 function Card() {
     return (
-        <Box tw="border rounded-lg">
+        <Box tw="border rounded-xl overflow-hidden" maxW="500px">
             <VStack align="start">
-                <VStack w="full" align="start" spacing={4}>
-                    <Box tw="border relative" w="100%" h="202.5px" bgColor="gray.100">
-                        {/* sizes=100% is needed here to make next/image work properly */}
-                        <Image src="/svg/avatar.svg" sizes="100%" layout="fill" objectFit="cover" />
-                    </Box>
-                    <HStack px={4} spacing={2}>
-                        <TabButton isActive />
-                        <TabButton />
-                        <TabButton />
-                        <TabButton />
-                        <TabButton />
-                    </HStack>
-                </VStack>
+                <ImageDisplay />
 
                 <VStack p={4} align="start" spacing={4}>
-                    <VStack align="start">
-                        <Text fontSize="xs" color="gray.500" mb={-2}>
-                            SOFTWARE ENGINEER (OR, USA)
-                        </Text>
-                        <Heading tw="text-3xl leading-none" letterSpacing={1} fontWeight="bold">
-                            Norman Gordon
-                        </Heading>
-                        <Text noOfLines={5} fontSize="sm" isTruncated>
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-                            tempor incididunt ut labore et dolore magna aliqua. Congue quisque
-                            egestas diam in arcu. At volutpat diam ut venenatis tellus in metus
-                            vulputate eu. Feugiat pretium nibh ipsum consequat.
-                        </Text>
-                    </VStack>
+                    <Text fontSize="xs" color="gray.500" mb={-2}>
+                        <span tw="text-teal-500">SOFTWARE ENGINEER</span> (OR, USA)
+                    </Text>
+                    <Text tw="leading-none" fontSize="3xl" fontFamily="Raleway" fontWeight="bold">
+                        Norman Gordon
+                    </Text>
+                    <Text noOfLines={5} fontSize="sm" color="gray.500" mb={4} isTruncated>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+                        tempor incididunt ut labore et dolore magna aliqua. Congue quisque egestas
+                        diam in arcu. At volutpat diam ut venenatis tellus in metus vulputate eu.
+                        Feugiat pretium nibh ipsum consequat.
+                    </Text>
 
-                    <VStack align="start">
-                        <Text textTransform="uppercase" fontSize="xs" fontWeight="bold">
-                            Availability
-                        </Text>
-                        <HStack>
-                            {["40 hours"].map((t) => (
-                                <Badge key={t} rounded="md" fontWeight="normal" color="gray.500">
-                                    {t}
-                                </Badge>
-                            ))}
-                        </HStack>
-                    </VStack>
-
-                    <VStack align="start">
-                        <Text textTransform="uppercase" fontSize="xs" fontWeight="bold">
-                            Focus
-                        </Text>
-                        <HStack>
-                            {["fontend", "react", "aws", "ui/ui"].map((t) => (
-                                <Badge key={t} rounded="md" fontWeight="normal" color="gray.500">
-                                    {t}
-                                </Badge>
-                            ))}
-                        </HStack>
-                    </VStack>
+                    <Attributes title="Availability" badges={["40 hours"]} />
+                    <Attributes
+                        title="Focus/skills"
+                        badges={["frontend", "ui/ux", "aws", "react"]}
+                    />
                 </VStack>
             </VStack>
         </Box>
@@ -84,18 +89,23 @@ function Card() {
 
 export default function Mentor() {
     return (
-        <HStack alignItems="stretch">
-            <Box minH={"100vh"} bgColor="gray.100" w="300px" flexShrink={0}></Box>
+        <Flex alignItems="stretch">
+            <Box
+                display={["none", "block"]}
+                minH={"100vh"}
+                bgColor="gray.100"
+                w="300px"
+                flexShrink={0}
+            />
             <Grid
-                alignItems="center"
-                templateColumns="repeat(auto-fit, minmax(360px, 1fr))"
-                w="full"
-                gap={8}
-                p={8}>
-                <Card />
-                <Card />
-                <Card />
+                tw="gap-8 w-full items-center"
+                p={[4, 8]}
+                justifyItems="center"
+                templateColumns="repeat(auto-fit, minmax(320px, 1fr))">
+                {times(3).map((i) => (
+                    <Card key={i} />
+                ))}
             </Grid>
-        </HStack>
+        </Flex>
     );
 }
