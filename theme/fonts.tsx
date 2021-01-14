614af6b8aab8bb11998678b5ca1b9b3eb3c496f0
Kawin <pechetrk@oregonstate.edu>
2021-01-14 12:24:01 +0700

    Added card component

---
+++ theme/fonts.tsx
@@ -0,0 +1,14 @@
+import { Global as GlobalStyle } from "@emotion/react";
+
+const fonts = () => (
+    <GlobalStyle
+        styles={`
+        @import url('https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700&display=swap');
+        @import url('https://fonts.googleapis.com/css2?family=Roboto+Slab:wght@300;400;500;700&display=swap');
+        @import url('https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;500;700&display=swap');
+        @import url('https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@300;400;500;700&display=swap');
+`}
+    />
+);
+
+export default fonts;
