970a887dd6bb19078c8a29247ad52530af9e9f76
Kawin <pechetrk@oregonstate.edu>
2021-01-14 20:18:57 +0700

    Updated mentor cover image with square image

--- theme/fonts.tsx
+++ theme/fonts.tsx
@@ -7,6 +7,7 @@ const fonts = () => (
         @import url('https://fonts.googleapis.com/css2?family=Roboto+Slab:wght@300;400;500;700&display=swap');
         @import url('https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;500;700&display=swap');
         @import url('https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@300;400;500;700&display=swap');
+        @import url('https://fonts.googleapis.com/css2?family=Raleway:wght@700&display=swap');
 `}
     />
 );
