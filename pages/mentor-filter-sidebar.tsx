8b2f8321a9a08fadbe6699bcc26b3b0706a12b95
Jett Seale <sealee@oregonstate.edu>
2021-01-22 22:00:22 -0800

    Created layout of mentor filter sidebar, filled with dummy data

---
+++ pages/mentor-filter-sidebar.tsx
@@ -0,0 +1,106 @@
+import "twin.macro";
+import { HStack, VStack, Text, Divider, Icon, Checkbox, Tag as Bubble } from "@chakra-ui/react";
+import { Sliders, DotsNine, Tag, CheckSquare, HourglassHigh, IconProps } from "phosphor-react";
+import React from "react";
+
+const FilterTitle = () => {
+    return (
+        <VStack align="start" paddingTop="20px" paddingLeft="10px">
+            <HStack>
+                <Icon tw="transform rotate-90" weight="bold" boxSize={30} as={Sliders} />
+                <Text fontSize="xl" fontWeight="bold">
+                    Filter
+                </Text>
+            </HStack>
+            <Divider orientation="horizontal" width="210px" />
+        </VStack>
+    );
+};
+
+const AverageReviews = () => {
+    return (
+        <VStack paddingLeft="10px" paddingBottom="20px">
+            <HStack>
+                <Icon weight="bold" boxSize={30} as={CheckSquare} />
+                <Text fontSize="sm" fontWeight="bold">
+                    Average Reviews
+                </Text>
+            </HStack>
+        </VStack>
+    );
+};
+
+type FilterComponentProps = {
+    title: string;
+    icon: React.ForwardRefExoticComponent<IconProps & React.RefAttributes<SVGSVGElement>>;
+    elements: { name: string; num: number }[];
+};
+
+const FilterComponent = (props: FilterComponentProps) => {
+    return (
+        <VStack paddingLeft="10px" align="start">
+            <HStack>
+                <Icon weight="bold" boxSize={30} as={props.icon} />
+                <Text fontSize="sm" fontWeight="bold">
+                    {props.title}
+                </Text>
+            </HStack>
+            {props.elements.map((element) => (
+                <VStack key={element.name} paddingLeft="6px" align="start">
+                    <HStack>
+                        <Checkbox colorScheme="purple">{element.name}</Checkbox>
+                        <Bubble size="sm" borderRadius="full">
+                            {element.num}
+                        </Bubble>
+                    </HStack>
+                </VStack>
+            ))}
+        </VStack>
+    );
+};
+
+type MentorFilterSidebarProps = {
+    categories: { category: string; numCats: number }[];
+    tags: { tag: string; numTags: number }[];
+    availabilities: { availability: string; numAvails: number }[];
+};
+
+const MentorFilterSidebar = (props: MentorFilterSidebarProps) => {
+    // Dummy data:
+    const categories = [
+        { name: "Software Engineering", num: 32 },
+        { name: "UI/UX", num: 20 },
+        { name: "Data Science", num: 12 },
+    ];
+
+    const tags = [
+        { name: "React", num: 35 },
+        { name: "SQL", num: 16 },
+        { name: "Python", num: 20 },
+    ];
+
+    const availabilities = [
+        { name: "30 hours", num: 2 },
+        { name: "20 hours", num: 12 },
+        { name: "10 hours", num: 22 },
+    ];
+
+    return (
+        <VStack
+            spacing={8}
+            h="85vh"
+            w="260px"
+            tw="border-gray-200 border-2 bottom-0"
+            position="fixed"
+            align="start"
+            overflow="auto">
+            <FilterTitle />
+            <FilterComponent title="Categories" icon={DotsNine} elements={categories} />
+            <FilterComponent title="Tags" icon={Tag} elements={tags} />
+            <FilterComponent title="Availability" icon={HourglassHigh} elements={availabilities} />
+            <AverageReviews />
+        </VStack>
+    );
+};
+
+export default MentorFilterSidebar;
