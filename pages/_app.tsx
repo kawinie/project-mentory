4a5c86b6a136ef1648f8db38a268700f1ffb1d57
ktsht <kitaa@oregonstate.edu>
2021-02-05 02:08:12 -0800

    Integrated Strapi
    Changed apolloClient by not using next-apollo

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,19 +2,26 @@ import { AppProps } from "next/app";
 import { ReactElement } from "react";
 import { GlobalStyles, theme as tailwindTheme } from "twin.macro";
 import { ChakraProvider } from "@chakra-ui/react";
+import { ApolloProvider } from "@apollo/client";
 
 import { ScreenProvider } from "hooks";
 import theme from "theme";
 import Fonts from "theme/fonts";
 
+import { useApollo } from "../utils/apollo";
+
 export default function MyApp({ Component, pageProps }: AppProps): ReactElement {
+    const apolloClient = useApollo(pageProps.initialApolloState);
+
     return (
-        <ScreenProvider screens={tailwindTheme`screens`}>
-            <GlobalStyles />
-            <Fonts />
-            <ChakraProvider theme={theme}>
-                <Component tw="debug-screens" {...pageProps} />
-            </ChakraProvider>
-        </ScreenProvider>
+        <ApolloProvider client={apolloClient}>
+            <ScreenProvider screens={tailwindTheme`screens`}>
+                <GlobalStyles />
+                <Fonts />
+                <ChakraProvider theme={theme}>
+                    <Component tw="debug-screens" {...pageProps} />
+                </ChakraProvider>
+            </ScreenProvider>
+        </ApolloProvider>
     );
 }
---
+++ services/strapi/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'e2574b45-9a8d-4373-9bd2-e6eae599da6b'
+};
