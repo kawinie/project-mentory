39fe1ee5ba610fb3e6c6ccca12210c5ed62f7fb4
ktsht <kitaa@oregonstate.edu>
2021-02-22 18:40:10 -0800

    Added social login

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -24,7 +24,7 @@ export default function MyApp({ Component, pageProps }: AppProps): ReactElement 
 
         if (token) {
             // authenticate the token on the server and place set user object
-            fetch(`${process.env.NEXT_PUBLIC_API_URL}users/me`, {
+            fetch(`${process.env.NEXT_PUBLIC_API_URL}/users/me`, {
                 headers: {
                     Authorization: `Bearer ${token}`,
                 },
--- services/strapi/config/server.js
+++ services/strapi/config/server.js
@@ -1,6 +1,7 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
+  url: env('', 'https://8a82866cf6eb.ngrok.io'),
   admin: {
     auth: {
       secret: env('ADMIN_JWT_SECRET', 'fcd760b844c60f8fb7edd97b429d7934'),
