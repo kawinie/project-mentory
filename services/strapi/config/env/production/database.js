48017c51dc0bb0d2767711d660977e0e26cdd6a5
ktsht <kitaa@oregonstate.edu>
2021-03-02 23:50:11 -0800

    Trying heroku

---
+++ services/strapi/config/env/production/database.js
@@ -0,0 +1,22 @@
+const { parseUri } = require('mysql-parse')
+const config = parseUri(process.env.DATABASE_URL);
+
+module.exports = ({ env }) => ({
+    defaultConnection: 'default',
+    connections: {
+      default: {
+        connector: 'bookshelf',
+        settings: {
+          client: "mysql",
+          host: env('DATABASE_HOST', config.host),
+          port: env('DATABASE_PORT', config.port),
+          database: env('DATABASE_NAME', config.database),
+          username: env('DATABASE_USERNAME', config.user),
+          password: env('DATABASE_PASSWORD', config.password),
+        },
+        options: {
+          useNullAsDefault: true,
+        },
+      },
+    },
+  });
