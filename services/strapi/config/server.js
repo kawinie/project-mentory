eb3885ca3b2f69af9de97d461828d4182106e32e
Kawin <pechetrk@oregonstate.edu>
2021-05-10 18:15:31 -0700

    [Patch] Remove SQLite3

--- services/strapi/config/server.js
+++ services/strapi/config/server.js
@@ -1,9 +1,9 @@
 module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 8082),
-  admin: {
-    auth: {
-      secret: env('ADMIN_JWT_SECRET', 'fcd760b844c60f8fb7edd97b429d7934'),
+    host: env("HOST", "0.0.0.0"),
+    port: env.int("PORT", 1337),
+    admin: {
+        auth: {
+            secret: env("ADMIN_JWT_SECRET", "fcd760b844c60f8fb7edd97b429d7934"),
+        },
     },
-  },
 });
