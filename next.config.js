8583bc79034a77e269aaa75921f4bbb7d6738a75
Kawin <pechetrk@oregonstate.edu>
2021-05-14 09:33:41 -0700

    Added image domain

--- next.config.js
+++ next.config.js
@@ -25,7 +25,7 @@ module.exports = withBundleAnalyzer({
         return config;
     },
     images: {
-        domains: ["localhost"],
+        domains: ["localhost", "https://floating-dusk-99867.herokuapp.com"],
     },
 
     // This is how to do redirections
