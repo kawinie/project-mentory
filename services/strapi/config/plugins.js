d4e102a7483cee14fd07723d8d21af2bb2dca4de
Kawin <pechetrk@oregonstate.edu>
2021-05-28 18:03:33 -0700

    Added email and UserInfo creation on registration

--- services/strapi/config/plugins.js
+++ services/strapi/config/plugins.js
@@ -1,13 +1,24 @@
 module.exports = ({ env }) => ({
     upload: {
-      provider: 'aws-s3',
-      providerOptions: {
-        accessKeyId: env('AWS_ACCESS_KEY_ID'),
-        secretAccessKey: env('AWS_ACCESS_SECRET'),
-        region: 'us-west-2',
-        params: {
-          Bucket: 'mentorystrapimedia',
+        provider: "aws-s3",
+        providerOptions: {
+            accessKeyId: env("AWS_ACCESS_KEY_ID"),
+            secretAccessKey: env("AWS_ACCESS_SECRET"),
+            region: "us-west-2",
+            params: {
+                Bucket: "mentorystrapimedia",
+            },
         },
-      },
+    },
+    email: {
+        provider: "sendgrid",
+        providerOptions: {
+            apiKey: env("SENDGRID_API_KEY"),
+        },
+        settings: {
+            defaultFrom: "pechetrk@oregonstate.edu",
+            defaultReplyTo: "pechetrk@oregonstate.edu",
+            testAddress: "kawin15@icloud.com",
+        },
     },
 });
